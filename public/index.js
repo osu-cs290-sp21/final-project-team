b00674b06234186d60ca2581932a02e8e854194d
theingmwe <theingmweoo.tmo@gmail.com>
2021-06-11 02:54:52 -0700

    added index file

--- public/index.js
+++ public/index.js
@@ -1,1 +1,21 @@
 // add button functionalities here
+
+/* function to show button */
+function showButton() {
+    var modalBG = document.getElementById("modal-background");
+    var addRecipeModal = document.getElementById("add-recipe-modal");
+
+    modalBG.classList.remove('hidden');
+    addRecipeModal.classList.remove("hidden");
+}
+
+/* event listener */
+window.addEventListener('DOMContentLoaded', function () {
+    // var recipe = document.getElementsByClassName("recipe");
+    
+    
+    var beepboop = document.getElementById("add-recipe-button");
+    if (beepboop) {
+        beepboop.addEventListener("click", showButton)
+    }
+})
